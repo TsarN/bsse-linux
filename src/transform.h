14dbc518131b398db12837cf20079f9159a94bfc
Nikita Tsarev <nikts53@gmail.com>
2021-04-23 17:55:18 +0300

    Fixes. Ugly ugly fixes.

--- src/transform.h
+++ src/transform.h
@@ -6,6 +6,8 @@
 #include <utility>
 #include <atomic>
 #include <vector>
+#include <memory>
+#include <functional>
 
 #include <unistd.h>
 #include <sys/mman.h>
@@ -15,7 +17,8 @@ auto TransformWithProcesses(
         std::ranges::contiguous_range auto &&range,
         std::regular_invocable<std::ranges::range_value_t<decltype(range)>> auto &&func,
         int nprocesses,
-        int taskSize = 1
+        int taskSize = 1,
+        bool balance = true
 ) -> std::ranges::output_range<decltype(func(std::declval<std::ranges::range_value_t<decltype(range)>>()))> auto {
     using OutType = decltype(func(std::declval<std::ranges::range_value_t<decltype(range)>>()));
 
@@ -23,27 +26,43 @@ auto TransformWithProcesses(
     auto taskCount = (n + taskSize - 1) / taskSize;
 
     struct State {
-        std::atomic<size_t> tasksTaken;
+        std::atomic<size_t> tasksTaken = 0;
     };
+
+    static_assert(std::atomic<size_t>::is_always_lock_free);
 
     const auto pageSize = getpagesize();
     const auto& align = [pageSize] (auto size) {
         return (size + pageSize - 1) / pageSize * pageSize;
     };
 
-    auto state = static_cast<State*>(mmap(nullptr, align(sizeof(State)), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0));
-    auto out = static_cast<OutType*>(mmap(nullptr, align(sizeof(OutType) * n), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0));
+    auto state = std::unique_ptr<State, std::function<void(State*)>>{
+        static_cast<State*>(mmap(nullptr, align(sizeof(State)), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0)),
+        [&](State* ptr) { munmap(ptr, align(sizeof(State))); }
+    };
+    if (!state) {
+        throw std::runtime_error{"mmap failed"};
+    }
+    new (state.get()) State{};
 
-    const auto& proc = [&] {
+    auto out = std::unique_ptr<OutType[], std::function<void(OutType*)>>{
+        static_cast<OutType*>(mmap(nullptr, align(sizeof(OutType) * n), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0)),
+        [&](OutType* ptr) { munmap(ptr, align(sizeof(OutType) * n)); }
+    };
+    if (!out) {
+        throw std::runtime_error{"mmap failed"};
+    }
+
+    const auto& proc = [&](auto takeTask) {
         while (true) {
-            auto task = state->tasksTaken.fetch_add(1);
-            if (task >= taskCount) {
+            auto task = takeTask();
+            if (task == -1) {
                 break;
             }
 
             auto inputBegin = std::ranges::data(range) + task * taskSize;
-            auto inputEnd = inputBegin + taskSize;
-            auto outputBegin = out + task * taskSize;
+            auto inputEnd = inputBegin + ((task == taskCount - 1 && n % taskSize > 0) ? (n % taskSize) : taskSize);
+            auto outputBegin = out.get() + task * taskSize;
             std::transform(inputBegin, inputEnd, outputBegin, func);
         }
     };
@@ -54,29 +73,39 @@ auto TransformWithProcesses(
         for (auto pid : pids) {
             waitpid(pid, nullptr, 0);
         }
-
-        munmap(state, align(sizeof(State)));
     };
 
     for (int i = 0; i < nprocesses; ++i) {
         auto pid = fork();
         if (pid < 0) {
             waitAll();
-            munmap(out, align(sizeof(OutType) * n));
             throw std::runtime_error{"fork failed"};
         }
 
         pids.push_back(pid);
 
         if (pid == 0) {
-            proc();
+            if (balance) {
+                proc([&]() -> int {
+                    auto task = state->tasksTaken.fetch_add(1);
+                    return (task >= taskCount) ? -1 : task;
+                }); 
+            } else {
+                const auto perWorker = (taskCount + nprocesses - 1) / nprocesses;
+                proc([&, perWorker, tasksTaken = perWorker * i]() mutable -> int {
+                    auto task = tasksTaken++;
+                    if (task >= taskCount || task >= perWorker * (i + 1)) {
+                        return -1;
+                    }
+                    return task;
+                });
+            }
             exit(0);
         }
     }
 
     waitAll();
 
-    std::vector<OutType> outVec(out, out + n);
-    munmap(out, align(sizeof(OutType) * n));
+    std::vector<OutType> outVec(out.get(), out.get() + n);
     return outVec;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -13,9 +13,11 @@ TEST_CASE("simple case") {
     std::vector<int> outputData(testData.size());
     std::transform(testData.begin(), testData.end(), outputData.begin(), transformer);
     
-    for (int taskSize : {1, 10, 100, 1000, 10000}) {
-        for (int processes : {1, 2, 4, 8, 16}) {
-            CHECK(TransformWithProcesses(testData, transformer, processes, taskSize) == outputData);
+    for (bool balance : {false, true}) {
+        for (int taskSize : {1, 7, 10, 429, 1000, 10000}) {
+            for (int processes : {1, 2, 4, 8, 16}) {
+                CHECK(TransformWithProcesses(testData, transformer, processes, taskSize, balance) == outputData);
+            }
         }
     }
 }
@@ -29,9 +31,11 @@ TEST_CASE("works with sleep") {
     std::vector<int> outputData(testData.size());
     std::transform(testData.begin(), testData.end(), outputData.begin(), [](int value) { return value * 10; });
     
-    for (int taskSize : {1, 10}) {
-        for (int processes : {1, 4, 16}) {
-            CHECK(TransformWithProcesses(testData, transformer, processes, taskSize) == outputData);
+    for (bool balance : {false, true}) {
+        for (int taskSize : {1, 10}) {
+            for (int processes : {1, 4, 16}) {
+                CHECK(TransformWithProcesses(testData, transformer, processes, taskSize, balance) == outputData);
+            }
         }
     }
 }
